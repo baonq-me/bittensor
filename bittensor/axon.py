57e0f1d6f04715f87b8738409a18cd97213886fe
quocbao <quocbao747@gmail.com>
2024-05-24 22:47:52 +0700

    Respect option --axon.ip when init axon object

--- bittensor/axon.py
+++ bittensor/axon.py
@@ -370,7 +370,7 @@ class axon:
         self.app = FastAPI()
         log_level = "trace" if bittensor.logging.__trace_on__ else "critical"
         self.fast_config = uvicorn.Config(
-            self.app, host="0.0.0.0", port=self.config.axon.port, log_level=log_level
+            self.app, host=self.config.axon.ip, port=self.config.axon.port, log_level=log_level
         )
         self.fast_server = FastAPIThreadedServer(config=self.fast_config)
         self.router = APIRouter()
